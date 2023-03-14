b2821be070b0ee3aa38474c2e65f42ac0ff296fc
Isaac Elliott <isaace71295@gmail.com>
2023-03-14 10:52:58 +1000

    add defaultPackage to flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,52 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    nix-filter.url = "github:numtide/nix-filter";
     ipso.url = "github:LightAndLight/ipso";
   };
-  outputs = { self, nixpkgs, flake-utils, ipso }:
+  outputs = { self, nixpkgs, flake-utils, nix-filter, ipso }:
     flake-utils.lib.eachDefaultSystem (system:
       let 
         pkgs = import nixpkgs { inherit system; };
-      in {
+      in rec {
+        packages = {
+          ipso = ipso.defaultPackage.${system};
+        };
+        
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
-            ipso.defaultPackage.${system}
+            packages.ipso
           ];
+        };
+        
+        defaultPackage = pkgs.stdenv.mkDerivation {
+          name = "timespent";
+          src = nix-filter.lib {
+            root = ./.;
+            include = [
+              "template.csv"
+              "timespent"
+            ];
+          };
+          
+          buildInputs = [
+            packages.ipso
+          ];
+          
+          buildPhase = "true";
+          
+          installPhase = ''
+            mkdir -p $out/bin
+            
+            # `patchShebangs` in `fixupPhase` only runs for executable files
+            chmod +x timespent 
+            cp timespent $out/bin
+            
+            cp template.csv $out
+
+            wrapProgram $out/bin/timespent \
+              --set TEMPLATE_FILE=$out/template.csv
+          ''; 
         };
       }
     );
