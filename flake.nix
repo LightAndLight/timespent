0077d0a710daf7e78b5fb5c535ec8b43a931df95
Isaac Elliott <isaace71295@gmail.com>
2023-03-14 11:30:22 +1000

    update ipso version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nix-filter.url = "github:numtide/nix-filter";
-    ipso.url = "github:LightAndLight/ipso";
+    ipso.url = "github:LightAndLight/ipso?tag=v0.5";
   };
   outputs = { self, nixpkgs, flake-utils, nix-filter, ipso }:
     flake-utils.lib.eachDefaultSystem (system:
